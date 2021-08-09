0b8e826ac48a898d614739c49cec90dab067cef5
abbasio <omarabbasi16@gmail.com>
2021-08-09 14:31:28 -0400

    Added search functionality and footer

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 const img = document.querySelector('img');
-const searchTerm = 'space';
-const apiURL = `https://api.giphy.com/v1/gifs/translate?api_key=H4sgeNgqOP913zfO1B6kbYJvZTYxyyeS&s=${searchTerm}`;
+const searchBar = document.querySelector('#gif-search');
+const container = document.querySelector('#container');
+container.textContent = "Enter a search term to load a random GIF!";
 
-fetch(apiURL, {mode: 'cors'})
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(response) {
-        const imgURL = response.data.images.original.url;
-        img.src = imgURL;
-    });
 
+function findRandomGif() {
+    searchTerm = searchBar.value;
+    const apiURL = `https://api.giphy.com/v1/gifs/translate?api_key=H4sgeNgqOP913zfO1B6kbYJvZTYxyyeS&s=${searchTerm}`;
+    
+    fetch(apiURL, {mode: 'cors'})
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(response) {
+            const imgURL = response.data.images.original.url;
+            img.src = imgURL;
+        });
+}
+
+
+   
