f1c6320bfe50526e10525c577ce6e0f669d48b0b
abbasio <omarabbasi16@gmail.com>
2021-08-13 13:38:29 -0400

    Rewrote with async/await

--- script.js
+++ script.js
@@ -4,19 +4,18 @@ const container = document.querySelector('#container');
 container.textContent = "Enter a search term to load a random GIF!";
 
 
-function findRandomGif() {
+async function findRandomGif() {
     searchTerm = searchBar.value;
     const apiURL = `https://api.giphy.com/v1/gifs/translate?api_key=H4sgeNgqOP913zfO1B6kbYJvZTYxyyeS&s=${searchTerm}`;
     
-    fetch(apiURL, {mode: 'cors'})
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(response) {
-            const imgURL = response.data.images.original.url;
-            img.src = imgURL;
-        });
+    const response = await fetch(apiURL, {mode: 'cors'});
+    const imgData = await response.json();
+    const imgURL = imgData.data.images.original.url;
+    img.src = imgURL;
 }
 
+// findRandomGif().catch(error => {
+//     console.error(error);
+// });
 
    
