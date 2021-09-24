adde7fe7e92b7481320f3fb9e9444beb3a21f237
abbasio <omarabbasi16@gmail.com>
2021-09-24 11:55:40 -0400

    Added enter key press functionality

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 const img = document.querySelector('img');
 const searchBar = document.querySelector('#gif-search');
-const container = document.querySelector('#container');
-container.textContent = "Enter a search term to load a random GIF!";
+const button = document.querySelector('#button');
 
+document.addEventListener("keyup", function(event){
+    if (event.key === 'Enter') button.click();
+});
 
 async function findRandomGif() {
     searchTerm = searchBar.value;
     const apiURL = `https://api.giphy.com/v1/gifs/translate?api_key=H4sgeNgqOP913zfO1B6kbYJvZTYxyyeS&s=${searchTerm}`;
     
+
     const response = await fetch(apiURL, {mode: 'cors'});
     const imgData = await response.json();
     const imgURL = imgData.data.images.original.url;
     img.src = imgURL;
 }
-
-// findRandomGif().catch(error => {
-//     console.error(error);
-// });
 
    
